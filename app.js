ea79e7c6f34a1e7c58198949b6d193673906ec40
aJetHorn <tohearn94@aol.com>
2015-01-18 02:46:09 -0500

    data sets

--- app.js
+++ app.js
@@ -205,7 +205,7 @@ $(document).ready( function () {
     	//console.log(calculateCorrelationCoefficient(data1, data2));
 	});
     $("#two").click(function() { //Y
-
+    	console.log(dataSource[12][1]);
     	//correlationColor(getCorrelationCoefficient(data1, data2));
 	});
 	$("#three").click(function() { //frequency
@@ -281,4 +281,136 @@ $(document).ready( function () {
     var base_url = "https://www.quandl.com/api/v1/";
     return base_url + content + auth_token_url + trail;
   }
+//Tags!
+
+	var dataTypes = [
+      "Bitcoin",
+      "Aluminum",
+      "Coal",
+      "Copper",
+      "Crude Oil",
+      "Milk",
+      "Beef",
+      "Cattle Hides",
+      "Broiler Chickens",
+      "Pork",
+      "Butter",
+      "Lamb",
+      "Wool",
+      "Salmon",
+      "Shrimp",
+      "Fishmeal",
+      "US Harbor Gasoline",
+      "Regular Gas",
+      "Midgrade Gas",
+      "Premium Gas",
+      "All Gas (Weighted)",
+      "Gold",
+      "Marcellus Natural Gas",
+      "Palladium",
+      "Platinum",
+      "Silver",
+      "Steel",
+      "Iron",
+      "Lead",
+      "Zinc",
+      "Tin",
+      "Iridium",
+      "Rhodium",
+      "Tobacco",
+      "Fed Funds Effective",
+      "4-week Treasury Bill",
+      "3-month Treasury Bill",
+      "6-month Treasury Bill",
+      "1-year Treasury Bill",
+      "Prime Rate",
+      "S&P 500 Index",
+      "Nasdaq 100 Index",
+      "Dow Jones Industrial Average",
+      "NYSE ARCE Major Market Index",
+      "NYSE ARCA Institutional Index",
+      "Nasdaq Composite Index",
+      "RUSSELL 3000 Index",
+      "Wilshire 5000 Index",
+      "NYSE Composite Index",
+      "Nasdaq Bank Index",
+      "Nasdaq Computer Index",
+      "Nasdaq Industrial Index",
+      "PHLX Housing Sector Index",
+      "Morgan Stanley Consumer Index",
+      "PHLX Oil Service Sector Index",
+      "NYSE AMEX Natural Gas Index",
+      "NYSE Amex Oil Index",
+      "TSX Global Gold Index",
+      "US Total Population (Thousands)",
+      "Stock"
+    ];
+    var dataSource = [
+      ["Bitcoin","BAVERAGE/USD"],
+      ["Aluminum","OFDP/ALUMINIUM_21"],
+      ["Coal","DOE/COAL"],
+      ["Copper","OFDP/COPPER_6"],
+      ["Crude Oil","OPEC/ORB"],
+      ["Milk","WSJ/MILK"],
+      ["Beef","ODA/PBEEF_USD"],
+      ["Cattle Hides","WSJ/HIDES"],
+      ["Broiler Chickens","WSJ/CHKN_NTL"],
+      ["Pork","ODA/PPORK_USD"],
+      ["Butter","WSJ/BUTTER"],
+      ["Lamb","ODA/PLAMB_USD"],
+      ["Wool","ODA/PWOOLC_USD"],
+      ["Salmon","ODA/PSALM_USD"],
+      ["Shrimp","ODA/PSHRI_USD"],
+      ["Fishmeal","ODA/PFISH_USD"]
+      ["US Harbor Gasoline","FRED/DGASNYH"],
+      ["Regular Gas","FRED/GASREGCOVW"],
+      ["Midgrade Gas","FRED/GASMIDCOVW"],
+      ["Premium Gas","FRED/GASPRMCOVW"],
+      ["All Gas (Weighted)","FRED/GASALLCOVW"],
+      ["Gold","LBMA/GOLD"],
+      ["Marcellus Natural Gas","WSJ/NG_MARC"],
+      ["Palladium","LPPM/PALL"],
+      ["Platinum","LPPM/PLAT"],
+      ["Silver","LBMA/SILVER"],
+      ["Steel","US WSJ/ST_SCRP"],
+      ["Iron","WSJ/FE_TJN"],
+      ["Lead","OFDP/LEAD_31"],
+      ["Zinc","OFDP/ZINC_26"],
+      ["Tin","OFDP/TIN_36"],
+      ["Iridium","JOHNMATT/IRID"],
+      ["Rhodium","JOHNMATT/RUTH"],
+      ["Tobacco","WORLDBANK/WLD_TOBAC_US"],
+      ["Fed Funds Effective","FRED/DFF.json"],
+      ["4-week Treasury Bill","FRED/DTB4WK"],
+      ["3-month Treasury Bill","FRED/DTB3"],
+      ["6-month Treasury Bill","FRED/DTB6"],
+      ["1-year Treasury Bill","FRED/DTB1YR"],
+      ["Prime Rate","FRED/DPRIME"],
+      ["S&P 500 Index","YAHOO/INDEX_GSPC"],
+      ["Nasdaq 100 Index","YAHOO/INDEX_GSPC"],
+      ["Dow Jones Industrial Average","NASDAQOMX/NDX"],
+      ["NYSE ARCE Major Market Index","BCB/UDJIAD1"],
+      ["NYSE ARCA Institutional Index","YAHOO/INDEX_XMI"],
+      ["Nasdaq Composite Index","NASDAQOMX/COMP"],
+      ["RUSSELL 3000 Index","YAHOO/INDEX_NYA"],
+      ["Wilshire 5000 Index","YAHOO/INDEX_RUA"],
+      ["NYSE Composite Index","YAHOO/INDEX_XMI"],
+      ["Nasdaq Bank Index","NASDAQOMX/BANK"],
+      ["Nasdaq Computer Index","YAHOO/INDEX_IIX"],
+      ["Nasdaq Industrial Index","NASDAQOMX/INDS"],
+      ["PHLX Housing Sector Index","YAHOO/INDEX_HGX"],
+      ["Morgan Stanley Consumer Index","YAHOO/INDEX_CYC"],
+      ["PHLX Oil Service Sector Index","NASDAQOMX/EPX"],
+      ["NYSE AMEX Natural Gas Index","YAHOO/INDEX_OSX"],
+      ["NYSE Amex Oil Index","YAHOO/INDEX_XNG"],
+      ["TSX Global Gold Index","YAHOO/INDEX_XAU"],
+      ["US Total Population (Thousands)","WORLDBANK/USA_SP_POP_TOTL"],
+      ["Stock"]
+    ];
+    $( "#tags" ).autocomplete({
+      source: dataTypes
+    });
+
+
+
 });
